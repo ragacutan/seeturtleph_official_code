8cc185151bf113c922c827e10d04f5bda46e96a4
Ram Adrian Gacutan <121160707+ragacutan@users.noreply.github.com>
2024-03-28 20:13:44 +0800

    Delete src/backend directory

--- src/backend/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "session.php";
-
-    if(array_key_exists("logout", $_GET) && $_GET['logout'] == true) {
-        session_destroy();
-        header("Location: /");
-    }
-?>
--- src/backend/session.php
+++
@@ -1,5 +0,0 @@
-<?php
-    if(!isset($_SESSION['id'])) {
-        session_start();
-    }
-?>
